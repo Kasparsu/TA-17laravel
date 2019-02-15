6598a8bd5cf37cc26f5c10cfb6a504160e85ce46
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2019-02-15 13:35:24 +0200

    change post seeder to generate comments with correct datetime

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -15,8 +15,13 @@ class PostSeeder extends Seeder
             factory(App\Post::class)->create()
                 ->each(function ($post) {
                     $n = rand(0, 10);
+                    $faker = Faker\Factory::create();
+                    $created_at = $faker->dateTimeBetween($post->created_at, 'now');
                     for($i=0; $i<$n; $i++) {
-                        $post->save(factory(App\Comment::class)->make());
+                        $post->save(factory(App\Comment::class)->make([
+                            'created_at' => $created_at,
+                            'updated_at' => $created_at
+                        ]));
                     }
             });
         }
